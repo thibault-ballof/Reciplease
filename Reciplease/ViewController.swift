b3ce0f643ab456c17cc9750020dcd5b9fc95387f
Thibault Ballof <moi@thibault-ballof.fr>
2022-03-21 11:01:15 +0100

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Thibault Ballof on 21/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
